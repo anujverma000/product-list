0add47c915f3c19a1663cd7297a6b8f7808d86bd
Anuj Verma <anujverma000@gmail.com>
2019-09-03 09:25:10 +0400

    fixed filter with multiple brands and types

--- server/resolvers/product/product.resolver.js
+++ server/resolvers/product/product.resolver.js
@@ -1,20 +1,20 @@
 const { DEFAULT_PAGE_SIZE }  = require('../../constants')
-const ProductRepository = require('./product-repository');
-const productRepository = new ProductRepository();
+const ProductRepository = require('./product-repository')
+const productRepository = new ProductRepository()
 
 module.exports = {
     products: (args) => {
-        let products = productRepository.findAll();
+        let products = productRepository.findAll()
         if(args.query){
             products = products.filter(p => p.name.indexOf(args.query) >= 0)
         }
         
-        if(args.brand){
-            products = products.filter(p => p.brand === args.brand)
+        if(args.brands){
+            products = products.filter(p => args.brands.includes(p.brand))
         }
 
-        if(args.type){
-            products = products.filter(p => p.type === args.type)
+        if(args.types){
+            products = products.filter(p => args.types.includes(p.type))
         }
 
         if(args.sort === "name_asc"){
@@ -34,10 +34,10 @@ module.exports = {
         }
         
         if(args.offset ||  args.limit){
-            let offset  = args.offset || 0;
+            let offset  = args.offset || 0
             let limit =  args.limit || DEFAULT_PAGE_SIZE
-            products =  products.slice(offset, offset + limit);
+            products =  products.slice(offset, offset + limit)
         }
-        return products;
+        return products
     }
 }
